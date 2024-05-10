1a376b6a35f10adf8793f9b5c93a704b3ec82021
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-05-11 02:38:47 +0530

    create user profile update api route

--- web/src/pages/Profile.jsx
+++ web/src/pages/Profile.jsx
@@ -1,5 +1,72 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+
+  const handleHange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <img
+          src={formData.avatar || currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          defaultValue={currentUser.username}
+          className="border p-3 rounded-lg "
+          onChange={handleHange}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          defaultValue={currentUser.email}
+          className="border p-3 rounded-lg "
+          onChange={handleHange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg "
+          onChange={handleHange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
+      </div>
+      <p className="text-red-700 my-5">{error ? error : ""}</p>
+      <p className="text-green-700 my-5">
+        {updateSuccess ? "User updated successfully!" : ""}
+      </p>
+    </div>
+  );
 }
