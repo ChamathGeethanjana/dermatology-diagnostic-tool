168f6bc547511ce7ae2d1297f520c72e17111d42
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-05-11 16:10:09 +0530

    add user profile delete functionality

--- web/src/pages/Profile.jsx
+++ web/src/pages/Profile.jsx
@@ -8,6 +8,9 @@ import {
   signOutUserStart,
   signOutUserSuccess,
   signOutUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from "../redux//user/userSlice";
 
 export default function Profile() {
@@ -54,6 +57,23 @@ export default function Profile() {
       dispatch(signOutUserSuccess());
     } catch (error) {
       dispatch(signOutUserFailure(error.message));
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
     }
   };
 
@@ -116,7 +136,12 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
         <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
           Sign out
         </span>
