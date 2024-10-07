40f14a551aefd53cefb398518384dea79874e940
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-10-07 23:50:49 +0530

    Refactor sign-in page layout and add image

--- web/src/pages/SignIn.jsx
+++ web/src/pages/SignIn.jsx
@@ -6,6 +6,7 @@ import {
   signInStart,
   signInSuccess,
 } from "../redux/user/userSlice";
+import sign_in_img from "../assets/signup-page-image.png";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -33,7 +34,7 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      console.log(data);
+      // console.log(data);
       if (data.success === false) {
         dispatch(signInFailure(data.message));
         return;
@@ -60,39 +61,46 @@ export default function SignIn() {
   };
 
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input
-          type="email"
-          placeholder="email"
-          className="border p-2 rounded-lg"
-          id="email"
-          onChange={handleChange}
-        />
-        <p className="text-red-500">{formErrors.email}</p>
-        <input
-          type="password"
-          placeholder="password"
-          className="border p-2 rounded-lg"
-          id="password"
-          onChange={handleChange}
-        />
-        <p className="text-red-500">{formErrors.password}</p>
-        <button
-          disabled={loading}
-          className="bg-sky-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-        >
-          {loading ? "Loading..." : "Sign In"}
-        </button>
-      </form>
-      <div className="flex gap-2 mt-5">
-        <p>Dont have an account?</p>
-        <Link to={"/sign-up"}>
-          <span className="text-blue-700">Sign up</span>
-        </Link>
+    <div className="p-3 max-w-lg mx-auto flex justify-center flex-col sm:flex-row gap-10">
+      <img
+        className="sm:h-[400px] drop-shadow-2xl my-auto p-5"
+        src={sign_in_img}
+        alt="sign up page image"
+      />
+      <div className="w-full sm:w-96">
+        <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="email"
+            placeholder="email"
+            className="border p-2 rounded-lg"
+            id="email"
+            onChange={handleChange}
+          />
+          <p className="text-red-500">{formErrors.email}</p>
+          <input
+            type="password"
+            placeholder="password"
+            className="border p-2 rounded-lg"
+            id="password"
+            onChange={handleChange}
+          />
+          <p className="text-red-500">{formErrors.password}</p>
+          <button
+            disabled={loading}
+            className="bg-sky-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            {loading ? "Loading..." : "Sign In"}
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p>Dont have an account?</p>
+          <Link to={"/sign-up"}>
+            <span className="text-blue-700">Sign up</span>
+          </Link>
+        </div>
+        {error && <p className="text-red-500 mt-5">{error}</p>}
       </div>
-      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
