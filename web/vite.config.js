f5c39c199c848163ef28aada7ef13d665996f498
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-10-07 23:51:27 +0530

    Refactor vite.config.js: Update server proxy target and add dotenv configuration

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+// import { loadEnv } from "vite";
+dotenv.config();
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  // const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_SERVER_BASE_URL,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
